94fd03dbe7850b281f97fdaeeb6bf3940edeb5ad
ShengHong Luo <LuoShenHong@outlook.com>
2024-07-24 09:13:36 +0800

    update-createroom

--- Services/CaptchaService.cs
+++ Services/CaptchaService.cs
@@ -4,19 +4,32 @@ namespace ScorerApp;
 
 public class CaptchaService
 {
-    const string chars = "abcdefghijkmnpqrstuvwxyz";
+    const string chars = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
     const string chars2 = "0123456789";
 
-    public string GenerateCodeAsync(int length = 5)
+    public string GenerateCodeAsync(int length = 6)
     {
         StringBuilder captchaBuilder = new StringBuilder();
         Random random = new Random();
-        for (int i = 0; i < 2; i++)
+
+        // Append a letter
+        int index = random.Next(chars.Length);
+        captchaBuilder.Append(chars[index]);
+
+        // Append numbers length - 1
+        for (int i = 0; i < length - 1; i++)
         {
-            int index = random.Next(chars.Length);
-            captchaBuilder.Append(chars[index]);
+            int index2 = random.Next(chars2.Length);
+            captchaBuilder.Append(chars2[index2]);
         }
-        
+
+        return captchaBuilder.ToString();
+    }
+
+    public string GenerateNuberCodeAsync(int length = 8)
+    {
+        StringBuilder captchaBuilder = new StringBuilder();
+        Random random = new Random();
         for (int i = 0; i < length; i++)
         {
             int index = random.Next(chars2.Length);
@@ -25,18 +38,5 @@ public class CaptchaService
 
         return captchaBuilder.ToString();
     }
-
-	public string GenerateNuberCodeAsync(int length = 8)
-	{
-		StringBuilder captchaBuilder = new StringBuilder();
-		Random random = new Random();
-		for (int i = 0; i < length; i++)
-		{
-			int index = random.Next(chars2.Length);
-			captchaBuilder.Append(chars2[index]);
-		}
-
-		return captchaBuilder.ToString();
-	}
 
 }
